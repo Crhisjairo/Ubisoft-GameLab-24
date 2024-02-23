a0800df2186bf99320ee8d85822a7dc6d03167ad
Christian Castaneda <crhisjairo@hotmail.com>
2024-02-22 22:17:18 -0500

    MinigameManager added

--- Assets/_Scripts/Managers/MinigamesManager.cs
+++ Assets/_Scripts/Managers/MinigamesManager.cs
@@ -1,102 +1,178 @@
-﻿using Assets._Scripts.Shared;
-using System;
+﻿using System;
 using System.Collections;
+using System.Diagnostics;
+using Assets._Scripts.Shared;
+using Cinemachine;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
-using UnityEngine.UIElements.Experimental;
+using Debug = UnityEngine.Debug;
 
-namespace Assets._Scripts.Managers
+namespace _Scripts.Managers
 {
     public class MinigamesManager : MonoBehaviour
     {
         [SerializeField] private MinigameSceneNames defaultScene;
 
-        [SerializeField] private Animator spinnerAnimator;
+        [Tooltip("Time in seconds for each scene to be played.")]
+        [SerializeField] private float timeForEachScene = 60f;
+        [Space(20)]
+        [SerializeField] private CanvasGroup uiCanvasGroup;
         [SerializeField] private Animator transitionCanvasAnim;
+        [SerializeField] private Animator spinnerAnimator;
+        
+        [SerializeField] private TextMeshProUGUI timerText;
+        
+        [Space(20)]
+        [SerializeField] private float startAnimTime = 3;
+        [SerializeField] private float waitTimeAfterLoading = 0.3f;
+        [SerializeField] private float endAnimTime = 0.3f;
 
-        [SerializeField] private float waitTimeBeforeLoading;
-        [SerializeField] private float waitTimeAfterLoading;
-
-        [SerializeField] private bool saveOnLoad = false;
-        [SerializeField] private float waitTimeForSaving = 5f;
-
+        [Space(20)]
         [SerializeField] private Slider loadingSlider;
         //TODO: Trigger the save data.
 
-        private Animator _loadingScreenAnimator;
+        [SerializeField] private CinemachineBrain brain;
+        [FormerlySerializedAs("cam")]
+        [SerializeField] private CinemachineVirtualCamera vCam;
+
         private Coroutine _loadingScreenRoutine;
 
         private int scenesCount;
+        private bool _isLoading;
+        
+        private float _loadProgress = 1f;
+        private float _loadBarsmoothness = 0.1f;
+
+        private AsyncOperation loading;
+        
+        public float tiempoInicial; // Tiempo inicial en segundos
+        private float tiempoRestante; // Tiempo actual restante
+
 
         private void Awake()
         {
-            SetComponents();
+            SetupComponents();
+            
             scenesCount = Enum.GetNames(typeof(MinigameSceneNames)).Length;
+        }
+        
+        private void SetupComponents()
+        {
+            uiCanvasGroup.alpha = 0;
+            spinnerAnimator.enabled = false;
+            transitionCanvasAnim.enabled = false;
+
+            tiempoRestante = tiempoInicial = timeForEachScene;
         }
 
         private void Start()
         {
-            DontDestroyOnLoad(gameObject);
-
-            StartLoadScreenAnimations(TransitionAnimations.FadeIn);
-        }
-
-        private void SetComponents()
-        {
-            _loadingScreenAnimator = GetComponent<Animator>();
+            StartLoadScreen(defaultScene.ToString());
         }
 
         public void StartLoadScreen(string sceneToLoad)
+        { 
+            _loadingScreenRoutine = StartCoroutine(LoadScreenAsync(sceneToLoad));
+        }
+
+        private void Update()
         {
-            if (_loadingScreenRoutine is null)
+            if (_isLoading)
             {
-                _loadingScreenRoutine = StartCoroutine(LoadScreenAsync(sceneToLoad));
+                UpdateLoadingBar();
+            }
+            else
+            {
+                // TODO: Start counter timer more precisely. Start after the loading screen is done.
+                UpdateCounterTimer();    
+            }
+            
+        }
+
+        private void UpdateLoadingBar()
+        {
+            if (loading is not null)
+            {
+                _loadProgress = loading.progress;
+                _loadBarsmoothness = 2f;
+            }
+                
+            float target = _loadProgress;
+            loadingSlider.value = Mathf.MoveTowards(loadingSlider.value, target, _loadBarsmoothness * Time.deltaTime);
+        }
+
+        private void UpdateCounterTimer()
+        {
+            // Verifica que el tiempo no sea negativo para evitar mostrar números negativos
+            if (tiempoRestante > 0)
+            {
+                // Decrementa el tiempo restante en cada fotograma
+                tiempoRestante -= Time.deltaTime;
+
+                int minutos = Mathf.FloorToInt(tiempoRestante / 60);
+                int segundos = Mathf.FloorToInt(tiempoRestante % 60);
+                int milisegundos = Mathf.FloorToInt((tiempoRestante * 1000) % 1000);
+                
+                // Actualiza el texto del contador en la interfaz de usuario
+                timerText.SetText(
+                    string.Format("{0:D2}:{1:D2}:{2:D2}", minutos, segundos, milisegundos)
+                );
+            }
+            else
+            {
+                // Puedes realizar acciones cuando el tiempo llega a cero
+                Debug.Log("¡Tiempo agotado!");
             }
         }
 
-        private IEnumerator LoadScreenAsync(string sceneToLoad, Action onRoutineStarts = null)
+        private IEnumerator LoadScreenAsync(string sceneToLoad)
         {
-            StartLoadScreenAnimations(TransitionAnimations.FadeIn);
+            _isLoading = true;
+         
+            SetActiveAnimators(true);
+            transitionCanvasAnim.Play(TransitionAnimations.FadeIn.ToString());
 
-            yield return new WaitForSecondsRealtime(waitTimeBeforeLoading);
-
-            AsyncOperation loading = SceneManager.LoadSceneAsync(sceneToLoad);
-            loading.completed += OnCompleteLoading;
-
+            yield return new WaitForSecondsRealtime(startAnimTime);
+            
+            loading = SceneManager.LoadSceneAsync(sceneToLoad, LoadSceneMode.Additive);
+            
             while (!loading.isDone)
             {
-                loadingSlider.value = loading.progress;
-
+                _loadProgress = loading.progress;
+                Debug.Log(_loadProgress);
+                
                 yield return null;
             }
-
+            
             yield return new WaitForSecondsRealtime(waitTimeAfterLoading);
+            // When the scene is loaded, fade out the transition canvas.
+            transitionCanvasAnim.Play(TransitionAnimations.FadeOut.ToString());
+            //yield return null;
 
-            FinishLoadScreenAnimations();
-            onRoutineStarts?.Invoke();
+            yield return new WaitForSecondsRealtime(endAnimTime);
+            
+            _isLoading = false;
 
-            yield return new WaitForSecondsRealtime(waitTimeForSaving);
-
-            Destroy(gameObject);
+            //brain.gameObject.SetActive(false);
+            vCam.gameObject.SetActive(false);
+            
+            SetActiveAnimators(false);
+            
+            //yield return null;
         }
-        private void StartLoadScreenAnimations(TransitionAnimations transition)
+        
+        private void SetActiveAnimators(bool isActive)
         {
-            spinnerAnimator.enabled = true;
-            transitionCanvasAnim.enabled = true;
-
-            _loadingScreenAnimator.Play(transition.ToString());
-        }
-
-        private void FinishLoadScreenAnimations()
-        {
-            spinnerAnimator.enabled = false;
-            transitionCanvasAnim.enabled = false;
+            spinnerAnimator.enabled = isActive;
+            transitionCanvasAnim.enabled = isActive;
         }
 
         private void OnCompleteLoading(AsyncOperation loading)
         {
-            _loadingScreenAnimator.Play(TransitionAnimations.FadeOut.ToString());
+            transitionCanvasAnim.Play(TransitionAnimations.FadeOut.ToString());
         }
     }
 }
