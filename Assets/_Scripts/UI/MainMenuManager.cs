aed9d22020d3107d326561bd7df86f0fb21dc053
Christian Castaneda <crhisjairo@hotmail.com>
2024-03-06 18:33:29 -0500

    Main menu setup

---
+++ Assets/_Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,14 @@
+﻿using Assets._Scripts.Shared;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace _Scripts.UI
+{
+    public class MainMenuManager : MonoBehaviour
+    {
+
+        
+        
+       
+    }
+}
