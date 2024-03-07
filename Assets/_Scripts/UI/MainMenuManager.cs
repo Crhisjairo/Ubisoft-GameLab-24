7d2de5c982569ad39c7557e6173662ce3d8eaa77
MGWilllin <90008306+MGWilllin@users.noreply.github.com>
2024-03-06 23:30:42 -0500

    3

--- Assets/_Scripts/UI/MainMenuManager.cs
+++ Assets/_Scripts/UI/MainMenuManager.cs
@@ -6,19 +6,9 @@ namespace _Scripts.UI
 {
     public class MainMenuManager : MonoBehaviour
     {
-        private Animator animator; // Example animator variable
 
-        // Start is called before the first frame update
-        void Start()
-        {
-            // Get the Animator component attached to this GameObject
-            animator = GetComponent<Animator>();
-
-            // Example: Play an animation
-            if (animator != null)
-            {
-                animator.Play("YourAnimationName");
-            }
-        }
+        
+        
+       
     }
 }
